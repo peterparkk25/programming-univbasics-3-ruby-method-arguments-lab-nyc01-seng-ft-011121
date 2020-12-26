f31188ec1e090f8f57f575cfa661935db9968b15
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-26 22:53:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,11 @@ end
 
 introduction_with_language("peter", "ruby")
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language_optional("peter")
 
 
 
